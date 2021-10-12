e3adb4970b88887d00ae4c9daa7c4836446d709a
942547 <942547@gmail.com>
2021-10-12 13:28:52 +0300

    clear and debug

--- themes/vreedom18-bingo/assets/js/script.js
+++ themes/vreedom18-bingo/assets/js/script.js
@@ -13,7 +13,7 @@
 
   // navbarDropdown
 	if ($(window).width() < 992) {
-		$('#navigation .dropdown-toggle').on('click', function () {
+		$('#navbarSupportedContent .dropdown-toggle').on('click', function () {
 			$(this).siblings('.dropdown-menu').animate({
 				height: 'toggle'
 			}, 300);
